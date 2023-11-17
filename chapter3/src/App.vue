eb7599f2eb29476ea787fec511a05b628ab9ce0d
Muhammad Nawaz <sardarnawaz122@gmail.com>
2023-11-17 09:22:30 +0500

    First Component of vue is created

--- chapter3/src/App.vue
+++ chapter3/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Hi Muhammad Nawaz Welcome to vue js"/>
+  <Home />
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Home,
   }
 }
 </script>
---
+++ chapter3/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <h1>Home compenent</h1>
+</template>
+<script>
+export default {
+    name: 'Home',
+
+
+
+}
+
+</script>
+<style scoped>
+h1{
+
+color:red;
+
+
+
+
+}
+</style>
