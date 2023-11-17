61bd872d6bdb4b375460884fc65b30175bc87780
Muhammad Nawaz <sardarnawaz122@gmail.com>
2023-11-17 09:56:16 +0500

    Lectur 7 work with interpolation and Data

--- chapter3/src/components/Home.vue
+++ chapter3/src/components/Home.vue
@@ -1,11 +1,27 @@
 <template>
 
-    <h1>Home compenent</h1>
+    <h1>Home compenent {{ "MuhammadNawaz".length }}</h1>
+    <h1>Email:{{ email }}</h1>
+    <h1>Password:{{ Mobile }}</h1>
+    <h1>Framwork:{{ getName() }}</h1>
+
+
 </template>
 <script>
 export default {
     name: 'Home',
+data(){
+    return {
 
+email :"sardarnawaz122@gmail.com",
+Mobile: 3435281821,
+getName(){
+return "Vue is popular front-end js framework"
+
+}
+    }
+    
+}
 
 
 }
