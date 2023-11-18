d7d5fe6670fa30c927861ab3d04366aea4413f98
Muhammad Nawaz <sardarnawaz122@gmail.com>
2023-11-18 17:27:29 +0500

    In this i worked with function and parameter

--- chapter3/src/components/Home.vue
+++ chapter3/src/components/Home.vue
@@ -1,36 +1,42 @@
 <template>
-
     <h1>Home compenent {{ "MuhammadNawaz".length }}</h1>
-    <h1>Email:{{ email }}</h1>
-    <h1>Mobile:{{ Mobile}}</h1>
-    <h1>Framwork:{{ getName() }}</h1>
-
-
+    <h1>Email:{{ getData().email }}</h1>
+    <h1>Mobile:{{ getData().Mobile }}</h1>
+    <h1>Framwork:{{ getName("I am good software Enginner") }}</h1>
 </template>
 <script>
 export default {
     name: 'Home',
-data(){
-    return {
+    data() {
+        return {
 
-email :"sardarnawaz122@gmail.com",
-Mobile: 3435281821,
-getName(){
-return "Vue is popular front-end js framework"
+            email: "sardarnawaz122@gmail.com",
+            Mobile: 3435281821,
 
-}
-    }
-    
-}
+        }
+    },
+    methods: {
+        getName(title) {
+            return title
+        },
+        getData() {
+            return {
+email:"Hafiz@gmail.com",
+Mobile:this.Mobile
+
+
+            }
+        }
+    },
 
 
 }
 
 </script>
 <style scoped>
-h1{
+h1 {
 
-color:red;
+    color: red;
 
 
 
